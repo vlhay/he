26643c42af6655b1017483ec55bd266e7b9b4912
vlhay <bloggernamnguyen@gmail.com>
2018-12-07 10:59:49 +0700

    Update a.php

--- a.php
+++ a.php
@@ -13,6 +13,21 @@
     <body>
 
 <?php
+	    
+ function md_keys_google($value){
+    mb_internal_encoding('UTF-8');
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'NokiaN73-2/3.0-630.0.2 Series60/3.0 Profile/MIDP-2.0 Configuration/CLDC-1.1');
+    curl_setopt($ch, CURLOPT_URL, 'http://www.google.com/search?ie=UTF-8&oe=utf8&q='.$value.'&btnG=Search');
+    $nd = curl_exec($ch);
+    curl_close($ch);
+    preg_match('#<div style="font-size:110%">Related:</div><div style="padding:4px 8px 0">(.*?)</div></div></div><div id="navbar" style="margin:4px 0;text-align:center">#is', $nd, $ab);$info = @$ab[1];
+    $info = str_replace('<a',',<a',$info);
+    $info =  strip_tags($info);
+    return html_entity_decode(substr($info,1,20000));
+    }
+	    
 if (!isset($_GET['url']))
 {
 echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><form method="get">Url: <input name="url" type="text"><input type="submit" value="Leech" ></form>';
@@ -63,7 +78,11 @@ echo '
   
         <form action="http://top18.viwap.com/namon" method="post">
     Tiêu đề:<br />  	
+    md_keys_google($value)
     <input name="ten" value="'.$title.'"><br />
+        tag:<br />  	
+    
+    <input name="tag" value="'.md_keys_google($title).'"><br />
     Thể loại:<br />  
     <select name="category">  
 		      		<optgroup label="Giải trí">	
